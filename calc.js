f93b2aeaad879cfa53cc76a3cb0e6c9e9cd3f9b8
victor-itedje <itedje309@gmail.com>
2023-11-24 13:56:03 -0800

    added del and cl

--- calc.js
+++ calc.js
@@ -1,4 +1,14 @@
 function calcNumbers(result){
+    console.log(result)
     form.displayresult.value=form.displayresult.value+result;
     
+
+}
+
+function clearInput(result){
+    form.displayresult.value = "";
+}
+
+function delete1(result){
+    form.displayresult.value = form.displayresult.value.slice(0,-1)
 }
